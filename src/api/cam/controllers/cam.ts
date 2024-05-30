3be5ed79acf4967bfc85b98b8276b5de1c2e9810
Mariano Fliri <riano.qx@gmail.com>
2024-05-30 20:04:14 -0300

    restrict cam access

--- src/api/cam/controllers/cam.ts
+++ src/api/cam/controllers/cam.ts
@@ -3,5 +3,33 @@
  */
 
 import { factories } from '@strapi/strapi'
+import { Context } from 'koa';
 
-export default factories.createCoreController('api::cam.cam');
+interface CustomQuery {
+  [key: string]: any;
+  filters?: {
+    [key: string]: any;
+  };
+}
+
+interface CustomContext extends Context {
+  query: CustomQuery;
+}
+
+export default factories.createCoreController('api::cam.cam',({strapi})=>({
+  async find(ctx: CustomContext){
+
+    const enclosureList = await strapi.entityService.findMany('api::enclosure.enclosure',{
+      filters:{
+        owner: ctx.state.user.id
+      }
+    })
+
+    ctx.query = { ...ctx.query, filters: { enclosure: enclosureList.map(e=>e.id) } };
+
+    const response = await super.find(ctx);
+    
+    return response
+  },
+}
+))
